cee2ad78ffff0374e9e1284b2c48a0307c1a11f5
Max Tsai <a22222448@gmail.com>
2024-03-08 12:13:09 +0800

    update content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
 			className='hero'
 			id='home'>
 			<div className='hero-banner'>
-				<h1>Backroads App</h1>
+				<h1>Backroads</h1>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
 					Vitae explicabo debitis est autem dicta.
